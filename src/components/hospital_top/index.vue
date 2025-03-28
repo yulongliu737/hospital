3dc1ae6c8d27408b2dba13ff781112371eba1cfd
yulong liu <m15672248461@163.com>
2025-03-28 15:05:02 +0800

    完成编写顶部样式

--- src/components/hospital_top/index.vue
+++ src/components/hospital_top/index.vue
@@ -3,11 +3,58 @@
 </script>
 
 <template>
-  <div>
-    top
+  <div class="top">
+    <div class="content">
+      <div class="top-left">
+        <img src="../../assets/images/logo.png">
+        <p>医脉通 预约挂号统一平台</p>
+      </div>
+      <div class="top-right">
+        <p class="help">帮助中心</p>
+        <p class="login">登录/注册</p>
+      </div>
+    </div>
   </div>
 </template>
 
 <style scoped>
-
+.top {
+  position: fixed;
+  z-index: 999;
+  width: 100%;
+  height: 70px;
+  background-color: white;
+  display: flex;
+  justify-content: center;
+  .content {
+    width: 1600px;
+    height: 70px;
+    background-color: white;
+    display: flex;
+    justify-content: space-between;
+    .top-left {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      img {
+        width: 50px;
+        height: 50px;
+        margin-right: 10px;
+      }
+      p {
+        font-size: 20px;
+        color: #55a6fe;
+      }
+    }
+    .top-right {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      color: #bbb;
+      p {
+        margin-right: 10px;
+      }
+    }
+  }
+}
 </style>

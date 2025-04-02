78f921712583e46d4e073f553629531ec53ac968
PC <m15672248461@163.com>
2025-04-02 10:45:58 +0800

    跳转详情增加传参

--- src/pages/home/search/index.vue
+++ src/pages/home/search/index.vue
@@ -1,38 +1,39 @@
 <script setup lang="ts">
-  import { Search } from '@element-plus/icons-vue'
-  import {ref} from "vue";
-  import type {HospitalInfo} from "@/api/home/type.ts";
-  import {reqHospitalInfo} from "@/api/home";
-  import {useRouter} from "vue-router";
-  let hosname = ref<string>('')
-  defineOptions({
-    name: "HospitalSearch"
-  })
+import {Search} from '@element-plus/icons-vue'
+import {ref} from "vue";
+import type {HospitalInfo} from "@/api/home/type.ts";
+import {reqHospitalInfo} from "@/api/home";
+import {useRouter} from "vue-router";
 
-  const fetchSuggestions = async (_:string, callback: any) => {
-    let hosInfos = await getHospitalInfos();
-    let showInfoMap = hosInfos?.data.map(item => {
-      return {
-        value: item.hosname,
-        hoscode: item.hoscode
-      }
-    })
-    // 执行此回调函数渲染数据
-    callback(showInfoMap)
-  }
+let hosname = ref<string>('')
+defineOptions({
+  name: "HospitalSearch"
+})
 
-  const getHospitalInfos = async () => {
-    let res: HospitalInfo = await reqHospitalInfo(hosname.value);
-    if (res.code === 200) {
-      return res;
+const fetchSuggestions = async (_: string, callback: any) => {
+  let hosInfos = await getHospitalInfos();
+  let showInfoMap = hosInfos?.data.map(item => {
+    return {
+      value: item.hosname,
+      hoscode: item.hoscode
     }
-  }
+  })
+  // 执行此回调函数渲染数据
+  callback(showInfoMap)
+}
 
-  let $router = useRouter()
-  // 点击某个推荐项目触发
-  const goDetail =  (item: any) => {
-    $router.push({path:'/hospital/register'})
+const getHospitalInfos = async () => {
+  let res: HospitalInfo = await reqHospitalInfo(hosname.value);
+  if (res.code === 200) {
+    return res;
   }
+}
+
+let $router = useRouter()
+// 点击某个推荐项目触发
+const goDetail = (item: any) => {
+  $router.push({path: '/hospital/register', query: {hoscode: item.hoscode}})
+}
 
 </script>
 
@@ -60,12 +61,14 @@
   display: flex;
   justify-content: center;
   align-items: center;
+
   .search-bar {
     width: 1000px;
     display: flex;
     justify-content: center;
     align-items: center;
     margin-top: 20px;
+
     .searchBtn {
       margin-left: 10px;
     }
